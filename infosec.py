cdda8880785bf645c9a7add0d2e0af3e8cb974dd
sayaliupasani1 <sayaliupasani@gmail.com>
2016-10-10 22:14:27 -0400

    initial commit

---
+++ infosec.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# Modified to implement Intrusion Detection System by Sayali Upasani.
+# December 2014- NYU polytechnic School of Engineering.
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+
+import csv
+from collections import defaultdict
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        flag = 0
+        columns = defaultdict(list)                                            
+        with open('/home/ubuntu/ryu/ryu/app/ids.txt') as f:                    
+            reader = csv.reader(f)                                             
+            reader.next()                                                      
+            for row in reader:               
+                for (i,v) in enumerate(row):                                 
+                    columns[i].append(v) 
+        print 'Columns[0]: %s'%columns[0][:]
+        print 'Columns[1]: %s'%columns[1][:]
+        print 'len: %s'%(reader.line_num-1)
+        
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        dst = eth.dst
+        src = eth.src
+        print eth.dst
+        print 'The Destination is %s'%dst
+
+        for i in range(0, int(reader.line_num-1)):                           
+            if (eth.src == columns[0][i] and eth.dst == columns[1][i]):
+                print 'Source & dest matched: %s, %s'%(src,dst)
+                print columns[0][i]
+            else:
+                flag = 1
+        print 'flag = %s'%flag
+        if (flag == 1):
+            dpid = datapath.id
+            self.mac_to_port.setdefault(dpid, {})
+
+            self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+            self.mac_to_port[dpid][src] = in_port
+
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+            else:
+                out_port = ofproto.OFPP_FLOOD
+
+            actions = [parser.OFPActionOutput(out_port)]
+
+            if out_port != ofproto.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+                self.add_flow(datapath, 1, match, actions)
+
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
+        else:
+            print 'No Communication Allowed!'
